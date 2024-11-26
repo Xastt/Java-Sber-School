2d1aefc2977d8967c08ebf8c284512b6f4b0e85a
Xast <artemhasrovyan@gmail.com>
2024-11-26 18:26:06 +0300

    Added TerminalRun, doesn't work correctly

--- src/main/java/ru/xast/sbertasks/task4/FirstTask/Main.java
+++ src/main/java/ru/xast/sbertasks/task4/FirstTask/Main.java
@@ -1,15 +1,13 @@
 package ru.xast.sbertasks.task4.FirstTask;
 
-import ru.xast.sbertasks.task4.FirstTask.Excpts.AccountIsLockedException;
-import ru.xast.sbertasks.task4.FirstTask.Excpts.InvalidPinException;
-
 public class Main {
     public static void main(String[] args) {
-        TerminalServer terminalServer = new TerminalServer(1000);
+        TerminalServer server = new TerminalServer(1000);
         PinValidator pinValidator = new PinValidator("1234");
-        Terminal terminal = new TerminalImpl(terminalServer, pinValidator);
-
-
-
+        TerminalImpl terminal = new TerminalImpl(server, pinValidator);
+        TerminalRun app = new TerminalRun(terminal);
+        app.run();
     }
+
+
 }
